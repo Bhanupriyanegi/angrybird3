d1bd5e3acb846e60cf9a9c58b55ecfe0bfa60ce8
Bhanupriyanegi <87189057+Bhanupriyanegi@users.noreply.github.com>
2021-08-20 18:28:30 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,16 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,100,100);
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    this.body.position.x = mouseX;
+    this.body.position.y = mouseY;
+    super.display();
+   
+       
+    
+    
+  }                                                                        
+}
